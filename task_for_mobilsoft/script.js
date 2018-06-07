cd05623271f8fe02696afd9d6fd61b3d7db05ae9
Kirill <kikuvasil@gmail.com>
2018-06-07 23:29:31 +0300

    Layout

---
+++ task_for_mobilsoft/script.js
@@ -0,0 +1,30 @@
+var count = 1;
+
+showImg(1);
+
+function present (x){
+
+	count = count + 1;
+	showImg(count);
+
+
+}
+
+function showImg (x){
+	var images = document.getElementsByTagName("img");
+
+if (x>images.length) {
+	count =1;
+};
+
+if (x<1) {
+	count = images.length;
+};
+
+	for (var i = 0; i < images.length; i++) {
+		images[i].style.display = "none";
+	};
+
+	images[count - 1].style.display = "block";
+}
+
